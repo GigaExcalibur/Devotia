eecf7fd44d8c3fe4a92aefb8f3ca6626ad33b683
GigaExcalibur <jlevitt8962@gmail.com>
2023-09-26 16:10:24 -0700

    IT WORKED FIRST TEY

--- EngineHacks/DevotiaHax/NewComputeUnitBattleDefense/NewComputeUnitBattleDefense.c
+++ EngineHacks/DevotiaHax/NewComputeUnitBattleDefense/NewComputeUnitBattleDefense.c
@@ -1,9 +1,9 @@
-#include "NewComputeBattleUnitDefense.h"
+#include "NewComputeUnitBattleDefense.h"
 
 void NewComputeBattleUnitDefense(struct BattleUnit* attacker, struct BattleUnit* defender) {
-	if (SkillTester(unit, SoulbladeIDLink))
+	if (SkillTester(defender, SoulbladeIDLink) && GetItemType(defender->weaponBefore) == 0)
 		attacker->battleDefense = (attacker->terrainResistance + attacker->unit.res + attacker->terrainDefense + attacker->unit.def) / 2;
-    else if (GetItemAttributes(defender->weapon) & IA_TARGETSRES)
+    else if (GetItemAttributes(defender->weapon) & (1 << 26))
         attacker->battleDefense = attacker->terrainResistance + attacker->unit.res;
     else
         attacker->battleDefense = attacker->terrainDefense + attacker->unit.def;
---
+++ EngineHacks/DevotiaHax/NewComputeUnitBattleDefense/NewComputeUnitBattleDefense.h
@@ -0,0 +1,6 @@
+#include "gbafe.h"
+
+void NewComputeBattleUnitDefense(struct BattleUnit* attacker, struct BattleUnit* defender);
+
+extern bool SkillTester(Unit* unit, u8 skillID)
+extern u8 SoulbladeIDLink;
